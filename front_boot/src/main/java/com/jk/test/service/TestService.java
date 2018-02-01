7d73d6b1e212f365f832dc1d0e4fdb923c7bce6e
anyuechao <anyuechao@126.com>
2018-02-01 10:59:13 +0800

    提交项目front

---
+++ front_boot/src/main/java/com/jk/test/service/TestService.java
@@ -0,0 +1,13 @@
+package com.jk.test.service;
+
+/* 项目名称：medical_front
+ * 类名称：Administrator
+ * 类描述：
+ * 创建人：安月朝 anyuechao@126.com
+ * 创建时间：2018/2/1 0001 10:47
+ * 修改人：安月朝 anyuechao@126.com
+ * 修改时间：2018/2/1 0001 10:47
+ * 修改备注：
+ */
+public interface TestService {
+}
---
+++ front_boot/src/main/java/com/jk/test/service/TestServiceImpl.java
@@ -0,0 +1,16 @@
+package com.jk.test.service;
+
+import org.springframework.stereotype.Service;
+
+/* 项目名称：medical_front
+ * 类名称：Administrator
+ * 类描述：
+ * 创建人：安月朝 anyuechao@126.com
+ * 创建时间：2018/2/1 0001 10:48
+ * 修改人：安月朝 anyuechao@126.com
+ * 修改时间：2018/2/1 0001 10:48
+ * 修改备注：
+ */
+@Service("testService")
+public class TestServiceImpl implements TestService {
+}
